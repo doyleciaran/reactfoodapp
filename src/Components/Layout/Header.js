b2558cdfa28c6bc132eee616d33f1728c145f7bd
Ciaran Doyle <c.doyle@3deo.com>
2021-12-30 10:25:45 +0000

    header

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import {Fragment} from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header} >
+                <h1>React Meals</h1>
+                <HeaderCartButton />
+            </header>
+            <div>
+                <img src={mealsImage} className={classes['main-image']} alt='scran'/>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
