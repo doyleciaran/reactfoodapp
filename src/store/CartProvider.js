67f38d8dffe5c6b0d2ce675e357eeac7ab2b2de0
Ciaran Doyle <c.doyle@3deo.com>
2022-01-02 16:23:33 +0000

    final solution

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -28,7 +28,24 @@ const cartReducer = (state, action) => {
         return {items: updatedItems, totalAmount: updatedTotalAmount}
     }
     if (action.type === 'REMOVE') {
-        return state;
+        
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price ;
+        let updatedItems;
+
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id)
+        } else {
+            const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount 
+        };
     }
 
     return defaultCartState;
